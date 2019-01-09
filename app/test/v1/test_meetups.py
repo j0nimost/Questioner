442a2228faafd02782bdf47ad9bd73a594b48a31
j0nimost <john.nyingi@live.com>
2019-01-09 11:56:25 +0300

    finishes-163046711 fixed errors in tests

--- app/test/v1/test_meetups.py
+++ app/test/v1/test_meetups.py
@@ -2,7 +2,7 @@ import unittest
 import json
 
 from app import create_app
-from ...api.v1.models.meetups import Meetups, meetups
+from ...api.v1.models.meetupmodel import Meetups, meetups
 
 
 class MeetupsTestCase(unittest.TestCase):
@@ -12,8 +12,6 @@ class MeetupsTestCase(unittest.TestCase):
         self.client = self.app.test_client()
 
         self.meetup = {
-            'id': 1,
-            'createdOn': '2019-01-09',
             'location': 'IHub',
             'images': [],
             'topic': 'Nairobi Go Meetup',
@@ -21,13 +19,22 @@ class MeetupsTestCase(unittest.TestCase):
             'tags': []
         }
 
-        meetups.append(self.meetup)
+        meetups.append({
+            'id': 1,
+            'createdOn': '2019-01-09',
+            'location': 'IHub',
+            'images': [],
+            'topic': 'Nairobi Go Meetup',
+            'happeningOn': '2019-01-26',
+            'tags': []
+        })
 
     def test_create_meetup(self):
         response = self.client.post('/api/v1/meetups',
                                     data=json.dumps(self.meetup),
                                     content_type='application/json')
         self.assertEqual(response.status_code, 201)
+        print('{} code'.format(response.status_code))
         self.assertIn('Nairobi Go Meetup', str(json.loads(response.data)))
 
     def test_create_meetup_badrequest(self):
@@ -35,5 +42,7 @@ class MeetupsTestCase(unittest.TestCase):
                                     data=json.dumps(self.meetup),
                                     content_type='application/xml')
         self.assertEqual(response.status_code, 400)
-        self.assertIn('Invalid request type', str(json.loads(response.data)))
+        self.assertIn('invalid request type', str(json.loads(response.data)))
 
+    def tearDown(self):
+        meetups.pop()
