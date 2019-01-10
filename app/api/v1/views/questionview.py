eaed5414799160a3bab7843e87f64aec7d93e77e
j0nimost <john.nyingi@live.com>
2019-01-11 01:29:15 +0300

    ft-163046774 added questions view

---
+++ app/api/v1/views/questionview.py
@@ -0,0 +1,28 @@
+from flask import request, jsonify, make_response
+from flask import Blueprint
+
+from ..models.questionmodel import Questions
+
+ques = Blueprint('ques', __name__, url_prefix='/api/v1')
+
+
+@ques.route('/questions', methods=['POST'])
+def post():
+    '''Creates Question'''
+    if request.json:
+        userid = request.json['userid']
+        meetupid = request.json['meetupid']
+        title = request.json['title']
+        body = request.json['body']
+
+        question_obj = Questions.create_question(userid, meetupid, title, body)
+        question = {
+            'status': 201,
+            'data': [question_obj]
+        }
+        response = jsonify(question)
+        response.status_code = 201
+        return response
+
+    else:
+        return make_response(jsonify({"message": "invalid request type"}), 400)
