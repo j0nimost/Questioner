38db4541dea8e459d0a2e4a67a7cf051d6ae246d
j0nimost <john.nyingi@live.com>
2019-01-15 09:32:54 +0300

    ft-163185575 registered comments schema

--- app/api/v1/utils/schema.py
+++ app/api/v1/utils/schema.py
@@ -1,10 +1,12 @@
 from ..models.meetupmodel import meetup_schema, rsvp_schema
 from ..models.questionmodel import question_schema, vote_schema
+from ..models.commentmodel import comment_schema
 
 
 config = {
     'meetup': meetup_schema,
     'rsvp': rsvp_schema,
     'question': question_schema,
-    'vote': vote_schema
+    'vote': vote_schema,
+    'comment': comment_schema
 }
