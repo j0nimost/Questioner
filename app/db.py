509b665e94955951fa3d5865290ebecb332145ad
j0nimost <john.nyingi@live.com>
2019-01-19 14:36:08 +0300

    ft-163184382 added delete data queries, removed init_test

--- app/db.py
+++ app/db.py
@@ -10,10 +10,40 @@ Setups all the required connections and creates tables
 def init():
     '''Set's up the connection'''
     connection_str = os.getenv('DATABASE_URL')
-    connection = os.getenv(connection_str)
+    connection = psycopg2.connect(connection_str)
     return connection
 
-meetups_tbl = '''CREATE TABLE IF NOT EXISTS meetup(
+
+def exec_queries(queries_: list):
+    '''Create the tables for testdb'''
+    db = init()
+    cur = db.cursor()
+
+    try:
+        for query in queries_:
+            cur.execute(query)
+    except Exception as e:
+        return e
+    finally:
+        db.commit()
+        cur.close()
+
+
+def delete_test():
+    '''Drop tables'''
+    usertbl = "DELETE FROM usertbl CASCADE;"
+    meetuptbl = "DELETE FROM meetup CASCADE;"
+    commenttbl = "DELETE FROM comment CASCADE;"
+    questiontbl = "DELETE FROM question CASCADE;"
+    rsvptbl = "DELETE FROM rsvp CASCADE;"
+
+    drop_queries = [usertbl, meetuptbl, questiontbl, commenttbl, rsvptbl]
+    return drop_queries
+
+
+def create_query():
+    '''Create Queries'''
+    meetups_tbl = '''CREATE TABLE IF NOT EXISTS meetup(
         id serial PRIMARY KEY NOT NULL,
         createdOn TIMESTAMP NOT NULL,
         topic VARCHAR(80) NOT NULL,
@@ -23,7 +53,7 @@ meetups_tbl = '''CREATE TABLE IF NOT EXISTS meetup(
         happeningOn TIMESTAMP NOT NULL
     );'''
 
-users_tbl = '''CREATE TABLE IF NOT EXISTS usertbl (
+    users_tbl = '''CREATE TABLE IF NOT EXISTS usertbl (
         id serial PRIMARY KEY NOT NULL,
         firstname VARCHAR(55) NOT NULL,
         lastname VARCHAR(55) NOT NULL,
@@ -34,7 +64,7 @@ users_tbl = '''CREATE TABLE IF NOT EXISTS usertbl (
         UNIQUE (email, username)
     );'''
 
-comments_tbl = '''CREATE TABLE IF NOT EXISTS comment(
+    comments_tbl = '''CREATE TABLE IF NOT EXISTS comment(
         id serial PRIMARY KEY NOT NULL,
         createdOn TIMESTAMP NOT NULL,
         userid INTEGER NOT NULL,
@@ -46,7 +76,7 @@ comments_tbl = '''CREATE TABLE IF NOT EXISTS comment(
          question(id) ON DELETE CASCADE
     );'''
 
-question_tbl = '''CREATE TABLE IF NOT EXISTS question(
+    question_tbl = '''CREATE TABLE IF NOT EXISTS question(
         id serial PRIMARY KEY NOT NULL,
         meetupid INTEGER NOT NULL,
         title VARCHAR(80) NOT NULL,
@@ -56,7 +86,7 @@ question_tbl = '''CREATE TABLE IF NOT EXISTS question(
         ON DELETE CASCADE
     );'''
 
-rsvp_tbl = '''CREATE TABLE IF NOT EXISTS rsvp(
+    rsvp_tbl = '''CREATE TABLE IF NOT EXISTS rsvp(
         id serial PRIMARY KEY NOT NULL,
         userid INTEGER NOT NULL,
         meetupid INTEGER NOT NULL,
@@ -66,3 +96,6 @@ rsvp_tbl = '''CREATE TABLE IF NOT EXISTS rsvp(
         CONSTRAINT rsvp_user_fk FOREIGN KEY (userid) REFERENCES usertbl(id)
         ON DELETE CASCADE
     );'''
+
+    queries = [users_tbl, meetups_tbl, question_tbl, comments_tbl, rsvp_tbl]
+    return queries
