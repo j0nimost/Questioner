877794954b6578a2b211bb2bda9a3a4280701341
j0nimost <john.nyingi@live.com>
2019-01-19 14:41:11 +0300

    ft-163184382 added jwt generator

---
+++ app/api/v2/utils/authorization.py
@@ -0,0 +1,35 @@
+import os
+import jwt
+import datetime
+
+
+def encode_jwt(userid):
+    '''creates an encoded jwt token'''
+    try:
+        exp = datetime.datetime.utcnow() + datetime.timedelta(days=0,
+                                                              hours=1)
+        creation = datetime.datetime.utcnow()
+
+        payload = {
+            "expiry": exp.isoformat(),
+            "created": creation.isoformat(),
+            "id": userid
+        }
+        token = jwt.encode(
+            payload,
+            os.getenv('SECRET'),
+            algorithm='HS256')
+        return token
+    except Exception as e:
+        return e
+
+
+def decode_jwt(token):
+    '''decode the jwt token'''
+    try:
+        payload = jwt.decode(token, os.getenv('SECRET'))
+        return payload['id']
+    except jwt.ExpiredSignatureError:
+        return "Token is expired please login"
+    except jwt.InvalidTokenError:
+        return "Invalid Token please login"
