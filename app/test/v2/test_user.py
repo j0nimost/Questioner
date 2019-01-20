fe0251328d22f4f2c0fa17f78c8d870d4903d5bd
j0nimost <john.nyingi@live.com>
2019-01-20 18:47:10 +0300

    ft-163184382 modified test to fail

--- app/test/v2/test_user.py
+++ app/test/v2/test_user.py
@@ -29,7 +29,7 @@ class AuthTestCase(unittest.TestCase):
                                     headers={
                                         "Content-Type": "application/json"})
         print(response.data)
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
         self.assertEqual('j0nimost', data['data'][0]['user'][0]['username'])
         self.assertIsNotNone(data['data'][0]['token'])
