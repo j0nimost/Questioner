d164edd72e16301ee447dc5a1ef2662e9d14607f
j0nimost <john.nyingi@live.com>
2019-01-22 12:48:58 +0300

    ft-163289311 added insert images tests

--- app/test/v2/test_meetup.py
+++ app/test/v2/test_meetup.py
@@ -2,7 +2,7 @@ import unittest
 import json
 
 from app import create_app
-from ...db import create_query, delete_test, exec_queries
+from ...db import create_query, delete_test, exec_queries, seed_meetup
 
 
 class MeetupTestCase(unittest.TestCase):
@@ -18,6 +18,10 @@ class MeetupTestCase(unittest.TestCase):
             "topic": "Nairobi Golang",
             "location": "Senteru plaza",
             "happeningOn": "2019-01-26"
+        }
+
+        self.images = {
+            "images": ['lop.png', 'zip.png']
         }
 
     def test_create_meetup(self):
@@ -43,13 +47,26 @@ class MeetupTestCase(unittest.TestCase):
     def test_create_meetup_missing(self):
         '''Test if meetup object is missing'''
         del self.meetup['happeningOn']
-        response = self.client.post('api/v2/meetups',
-                                    data=json.dumps(self.meetup),
-                                    content_type='application/json')
+        response = self.client.patch('api/v2/meetups',
+                                     data=json.dumps(self.meetup),
+                                     content_type='application/json')
         self.assertEqual(response.status_code, 400)
         data = json.loads(response.data)
         self.assertEqual("unexpected 'happeningOn' is a required property",
                          data['error'])
+
+    def test_create_meetup_images(self):
+        '''Test image insertion'''
+        # Get seed
+        id_ = seed_meetup()
+        print('id is {}'.format(str(id_)))
+        response = self.client.post('''api/v2/meetups/{}/images
+                                    '''.format(str(id_)),
+                                    data=json.dumps(self.images),
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        data = json.loads(response.data)
+        self.assertEqual('Nairobi Go', data['data']['topic'])
 
     def tearDown(self):
         queries = delete_test()
