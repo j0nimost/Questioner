b3cb30ca25d0e25ccbf8fbb3e96cecc4366a9cc2
j0nimost <john.nyingi@live.com>
2019-01-22 12:48:35 +0300

    ft-163289311 added meetup schema

--- app/api/v2/utils/schema.py
+++ app/api/v2/utils/schema.py
@@ -1,8 +1,9 @@
 from ..models.usermodel import user_schema, login_schema
-from ..models.meetupmodel import meetup_schema
+from ..models.meetupmodel import meetup_schema, image_schema
 
 config = {
     'user': user_schema,
     'login': login_schema,
-    'meetup': meetup_schema
+    'meetup': meetup_schema,
+    'images': image_schema
 }
