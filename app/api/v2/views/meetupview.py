5bd4e304ad2cc4b307d597e817ed977e149e8a8e
j0nimost <john.nyingi@live.com>
2019-01-22 12:49:20 +0300

    ft-163289311 added images endpoint

--- app/api/v2/views/meetupview.py
+++ app/api/v2/views/meetupview.py
@@ -27,3 +27,25 @@ def post():
         "data": [meetup_dict]
     }
     return jsonify(meetup_dict), 201
+
+
+@meetup_v2.route('/meetups/<int:meetup_id>/images', methods=['PATCH'])
+@validate_input('images')
+def post_images(meetup_id):
+    images_ = request.json['images']
+    # doess meetup exist
+    meetup = meetup_obj.fetch('id', meetup_id)
+    if meetup:
+        '''update meetup'''
+        id_ = meetup_obj.insert_images(meetup_id, images=images_)
+        meetup_vals = meetup_obj.fetch('id', id_)
+        meetup_keys = ['id', 'createdOn', 'topic', 'location', 'images',
+                       'tags', 'happeningOn']
+        meetup_dict = (zip(meetup_keys, meetup_vals))
+        return jsonify(meetup_dict), 202
+    else:
+        notfound = {
+            "status": 404,
+            "error": "Not Found"
+        }
+        return jsonify(notfound), 404
