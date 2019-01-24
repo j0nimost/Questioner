fdb18a8f8e66f2801e4548455d586d098c893971
j0nimost <john.nyingi@live.com>
2019-01-24 12:17:01 +0300

    ch-163433181 added auth decorator

--- app/api/v2/views/meetupview.py
+++ app/api/v2/views/meetupview.py
@@ -1,6 +1,7 @@
 from flask import jsonify, request
 from flask import Blueprint
 
+from ..utils.authorization import isAuthorized
 from ..models.meetupmodel import MeetupModel
 from ..utils.validation import validate_input
 meetup_v2 = Blueprint('meetup_v2', __name__, url_prefix='/api/v2')
@@ -9,22 +10,26 @@ meetup_obj = MeetupModel()
 
 @meetup_v2.route('/meetups', methods=['POST'])
 @validate_input('meetup')
+@isAuthorized("admin")
 def post():
     ''''create a meetup'''
     topic = request.json['topic']
     location = request.json['location']
     happeningOn = request.json['happeningOn']
 
+    exists = meetup_obj.exists('topic', topic)
+    if exists:
+        error_obj = {
+            "status": 409,
+            "error": "topic exists"
+        }
+        return jsonify(error_obj), 409
     id_ = meetup_obj.insert_meetup_query(topic, location, happeningOn)
-    meetup_vals = meetup_obj.fetch('id', id_)
-    meetup_keys = ['id', 'createdOn', 'topic', 'location', 'images',
-                   'tags', 'happeningOn']
-
-    meetup_dict = dict(zip(meetup_keys, meetup_vals))
+    meetup_ = meetup_obj.fetch('id', id_)
     # create response body
     meetup_dict = {
         "status": 201,
-        "data": [meetup_dict]
+        "data": [meetup_]
     }
     return jsonify(meetup_dict), 201
 
