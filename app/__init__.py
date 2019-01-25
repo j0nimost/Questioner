632d4a17ccd4d3976b1df1020eca639da46e56d6
j0nimost <john.nyingi@live.com>
2019-01-25 06:31:48 +0300

    start-163371700 added tests

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from .api.v1.views.commentview import comment
 
 from .api.v2.views.userview import auth
 from .api.v2.views.meetupview import meetup_v2 as meetupv2
-
+from .api.v2.views.questionview import ques_v2
 
 def create_app(config):
     '''Creates all Flask configurations and returns app.
@@ -23,6 +23,7 @@ def create_app(config):
     app.url_map.strict_slashes = False
     app.register_blueprint(auth)
     app.register_blueprint(meetupv2)
+    app.register_blueprint(ques_v2)
     app.register_blueprint(meetupreq)
     app.register_blueprint(ques)
     app.register_blueprint(comment)
