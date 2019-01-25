2a1742129b87ca541634d7f6e619d4a4a8badfc5
j0nimost <john.nyingi@live.com>
2019-01-25 06:32:26 +0300

    ft-163371700 added custom query db

---
+++ db.sql
@@ -0,0 +1,59 @@
+CREATE TABLE IF NOT EXISTS roles(
+        id serial NOT NULL,
+        role VARCHAR(20) NOT NULL,
+        CONSTRAINT roles_pk PRIMARY KEY (id, role),
+        UNIQUE (role)
+    );
+
+CREATE TABLE IF NOT EXISTS usertbl (
+        id serial PRIMARY KEY NOT NULL,
+        firstname VARCHAR(55) NOT NULL,
+        lastname VARCHAR(55) NOT NULL,
+        username VARCHAR(55) NOT NULL,
+        email VARCHAR(55) NOT NULL,
+        password VARCHAR(100) NOT NULL,
+        userrole VARCHAR(20) NOT NULL,
+        createOn TIMESTAMP NOT NULL,
+        UNIQUE (email, username),
+        CONSTRAINT role_fk FOREIGN KEY (userrole) REFERENCES roles(role)
+    );
+
+
+CREATE TABLE IF NOT EXISTS meetup(
+        id serial PRIMARY KEY NOT NULL,
+        userid INTEGER,
+        createdOn TIMESTAMP NOT NULL,
+        topic VARCHAR(80) NOT NULL,
+        location VARCHAR(55) NOT NULL,
+        images TEXT[],
+        tags TEXT[],
+        happeningOn TIMESTAMP NOT NULL,
+        CONSTRAINT userid_fk FOREIGN KEY (userid) REFERENCES usertbl(id)
+        ON DELETE CASCADE,
+        UNIQUE(topic)
+    );
+
+CREATE TABLE IF NOT EXISTS question(
+        id serial PRIMARY KEY NOT NULL,
+        meetupid INTEGER NOT NULL,
+        userid INTEGER NOT NULL,
+        title VARCHAR(80) NOT NULL,
+        body VARCHAR(140) NOT NULL,
+        votes INTEGER,
+        CONSTRAINT ques_meetup_fk FOREIGN KEY (meetupid) REFERENCES meetup(id)
+        ON DELETE CASCADE,
+        CONSTRAINT userid_fk FOREIGN KEY (userid) REFERENCES usertbl(id)
+        ON DELETE CASCADE
+    );
+
+CREATE TABLE IF NOT EXISTS comment(
+        id serial PRIMARY KEY NOT NULL,
+        createdOn TIMESTAMP NOT NULL,
+        userid INTEGER NOT NULL,
+        questionid INTEGER NOT NULL,
+        body VARCHAR(140) NOT NULL,
+        CONSTRAINT userid_fk FOREIGN KEY (userid) REFERENCES usertbl(id)
+        ON DELETE CASCADE,
+        CONSTRAINT comment_question_fk FOREIGN KEY (questionid) REFERENCES
+         question(id) ON DELETE CASCADE
+    );
