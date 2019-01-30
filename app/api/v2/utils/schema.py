f97d7ba8467a0b135e014df0a7054ffe1a0d1961
j0nimost <john.nyingi@live.com>
2019-01-31 01:20:04 +0300

    ft-163289243 added tags to schema

--- app/api/v2/utils/schema.py
+++ app/api/v2/utils/schema.py
@@ -1,9 +1,10 @@
 from ..models.usermodel import user_schema, login_schema
-from ..models.meetupmodel import meetup_schema, image_schema
+from ..models.meetupmodel import meetup_schema, image_schema, tags_schema
 
 config = {
     'user': user_schema,
     'login': login_schema,
     'meetup': meetup_schema,
-    'images': image_schema
+    'images': image_schema,
+    'tags': tags_schema
 }
