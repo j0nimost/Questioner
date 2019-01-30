d5e8e80d89f8e53deb106263f3c272c3c9eb0a44
j0nimost <john.nyingi@live.com>
2019-01-31 01:21:32 +0300

    finishes-163289243 added tags endpoint and isauthorized decorator

--- app/api/v2/views/meetupview.py
+++ app/api/v2/views/meetupview.py
@@ -1,6 +1,7 @@
 from flask import jsonify, request
 from flask import Blueprint
 
+from ..utils.authorization import isAuthorized
 from ..models.meetupmodel import MeetupModel
 from ..utils.validation import validate_input
 meetup_v2 = Blueprint('meetup_v2', __name__, url_prefix='/api/v2')
@@ -9,28 +10,33 @@ meetup_obj = MeetupModel()
 
 @meetup_v2.route('/meetups', methods=['POST'])
 @validate_input('meetup')
+@isAuthorized("admin")
 def post():
     ''''create a meetup'''
     topic = request.json['topic']
     location = request.json['location']
     happeningOn = request.json['happeningOn']
 
+    exists = meetup_obj.exists('topic', topic)
+    if exists:
+        error_obj = {
+            "status": 409,
+            "error": "topic exists"
+        }
+        return jsonify(error_obj), 409
     id_ = meetup_obj.insert_meetup_query(topic, location, happeningOn)
-    meetup_vals = meetup_obj.fetch('id', id_)
-    meetup_keys = ['id', 'createdOn', 'topic', 'location', 'images',
-                   'tags', 'happeningOn']
-
-    meetup_dict = dict(zip(meetup_keys, meetup_vals))
+    meetup_ = meetup_obj.fetch('id', id_)
     # create response body
     meetup_dict = {
         "status": 201,
-        "data": [meetup_dict]
+        "data": [meetup_]
     }
     return jsonify(meetup_dict), 201
 
 
 @meetup_v2.route('/meetups/<int:meetup_id>/images', methods=['PATCH'])
-@validate_input('images')
+@validate_input('tags')
+@isAuthorized("admin")
 def post_images(meetup_id):
     images_ = request.json['images']
     # doess meetup exist
@@ -40,15 +46,50 @@ def post_images(meetup_id):
         id_ = meetup_obj.insert_images(meetup_id, images=images_)
         if isinstance(id_, bool):
             meetup_vals = meetup_obj.fetch('id', meetup_id)
-            meetupid = meetup_vals[0]
-            topic = meetup_vals[2]
-            images = meetup_vals[4]
+            meetupid = meetup_vals['id']
+            topic = meetup_vals['topic']
+            images = meetup_vals['images']
             meetup_dict = {
                 "status": 202,
                 "data": {
                     "meetup_id": meetupid,
                     "topic": topic,
                     "images": images
+                }
+            }
+            return jsonify(meetup_dict), 202
+        else:
+            return id_, 500
+    else:
+        notfound = {
+            "status": 404,
+            "error": "Not Found"
+        }
+        return jsonify(notfound), 404
+
+
+@meetup_v2.route("/meetups/<int:meetup_id>/tags", methods=['PATCH'])
+@validate_input("tags")
+@isAuthorized("admin")
+def post_tags(meetup_id):
+    '''Adds tags'''
+    tags = request.json['tags']
+    # doess meetup exist
+    meetup = meetup_obj.fetch('id', meetup_id)
+    if meetup:
+        '''update meetup'''
+        id_ = meetup_obj.insert_tags(meetup_id, tags=tags)
+        if isinstance(id_, bool):
+            meetup_vals = meetup_obj.fetch('id', meetup_id)
+            meetupid = meetup_vals['id']
+            topic = meetup_vals['topic']
+            tags = meetup_vals['tags']
+            meetup_dict = {
+                "status": 202,
+                "data": {
+                    "meetup_id": meetupid,
+                    "topic": topic,
+                    "tags": tags
                 }
             }
             return jsonify(meetup_dict), 202
