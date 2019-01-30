ff67cd95639f0d09575c0d818a96162b315275c7
j0nimost <john.nyingi@live.com>
2019-01-31 01:10:45 +0300

    ft-163289243 added auto generate script

--- script.py
+++ script.py
@@ -1,11 +1,24 @@
+import os
 from app.db import create_query, exec_queries, seed
 
 
 def create():
+    
     print("\n\t===============>")
     print("\n\t Create Script ")
     print("\n\t<================")
+    
     try:
+        os.system("""psql -c "CREATE USER questioner WITH
+                PASSWORD 'andela1';" -U postgres""")
+        print("## user created")
+        os.system("""psql -c 'CREATE DATABASE IF
+                NOT EXISTS qtest;' -U postgres""")
+        print("### database created")
+        os.system("""psql -c "GRANT ALL privileges on database
+                 qtest to questioner;" -U postgres""")
+        print("#### connection complete")
+
         queries = create_query()
         exec_queries(queries)
         print("\nsuccessfully created tables")
