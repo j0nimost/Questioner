240e46b0abac056b79ff024fa5ccc4596da028ec
j0nimost <john.nyingi@live.com>
2019-02-13 20:13:52 +0300

    ft-163940855 added two tests for validation

--- app/test/v2/test_rsvp.py
+++ app/test/v2/test_rsvp.py
@@ -3,7 +3,7 @@ import unittest
 import json
 
 from app import create_app
-from ...db import get_db
+from ...db import get_db, drop_tables
 
 
 class RSVPTestCase(unittest.TestCase):
@@ -60,3 +60,38 @@ class RSVPTestCase(unittest.TestCase):
         rsvp = json.loads(rsvp_res.data)
         self.assertEqual(rsvp['message'],
                          'Successfully added to RSVP')
+
+    def test_notfound_meetup(self):
+        '''Test if meetup does not exist'''
+        rsvp_res = self.client.post(
+            'api/v2/meetups/0/rsvp',
+            headers=self.auth_header)
+        self.assertEqual(rsvp_res.status_code, 404)
+        rsvp = json.loads(rsvp_res.data)
+        self.assertEqual(rsvp['error'], 'Not Found')
+
+    def test_conflict_rsvp(self):
+        '''Test conflicting rsvps'''
+        meetup_res = self.client.post('api/v2/meetups',
+                                      data=json.dumps(self.meetup),
+                                      headers=self.auth_header)
+        self.assertEqual(meetup_res.status_code, 201)
+        meetup = json.loads(meetup_res.data)
+        meetupid = meetup['data'][0]['id']
+
+        rsvp_res = self.client.post(
+            'api/v2/meetups/{}/rsvp'.format(meetupid),
+            headers=self.auth_header)
+        self.assertEqual(rsvp_res.status_code, 202)
+
+        rsvp_error = self.client.post(
+            'api/v2/meetups/{}/rsvp'.format(meetupid),
+            headers=self.auth_header)
+
+        self.assertEqual(rsvp_error.status_code, 409)
+        error = json.loads(rsvp_error.data)
+        self.assertEqual(error['error'], "Reservation already exists")
+
+    def tearDown(self):
+        with self.app.app_context():
+            drop_tables()
