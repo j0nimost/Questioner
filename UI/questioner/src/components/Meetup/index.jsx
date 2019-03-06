c09462419f444b7010da50a29e160cc4bec5d6cd
j0nimost <john.nyingi@live.com>
2019-03-06 16:21:39 +0300

    ft-164428065 added meetup component

---
+++ UI/questioner/src/components/Meetup/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { browserHistory, Link } from 'react-router'
+import axios from 'axios'
+import { Cookies } from 'react-cookie'
+import './meetup.css'
+import '../styles/style.css'
+
+
+class Meetup extends Component{
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    render(){
+        return(null)
+    }
+}
