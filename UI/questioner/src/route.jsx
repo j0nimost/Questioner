a5a1de38ca93730b53b87fbec75f014810b2259c
j0nimost <john.nyingi@live.com>
2019-03-06 13:19:14 +0300

    ft-164329280 added Login page to route

--- UI/questioner/src/route.jsx
+++ UI/questioner/src/route.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './App'
 import RegisterUser from './components/Register/index'
+import LoginUser from './components/Login/index'
 
 export default(
     <Route path='/' component={App}>
     <IndexRoute component={RegisterUser} />
+    <Route path='/login' component={LoginUser} />
     <Route path='*' component={RegisterUser} />
   </Route>
 )
